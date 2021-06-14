dc3732c9adb334d52915a262e4e8f61aa423d2be
ivanpradeep <pradepkrishnan@gmail.com>
2021-06-14 19:24:57 +0530

    Initial

---
+++ src/main/java/com/example/hellospring/GreetController.java
@@ -0,0 +1,15 @@
+package com.example.hellospring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+
+public class GreetController {
+    @RequestMapping("/hello")
+    public String greetings(){
+        return "greetings";
+    }
+}
+
